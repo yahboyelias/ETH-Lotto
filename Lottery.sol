2442656bc372189ffe422760722a0f5b4d71bd39
Elias <54425552+yahboyelias@users.noreply.github.com>
2022-09-04 15:42:01 -0400

    Update to the lottery
    Updated the solidity version along with all the necessary fixes.

--- Lottery.sol
+++ Lottery.sol
@@ -1,26 +1,26 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.8.16;
 
 contract Lottery {
     address public Admin;
-    address[] public players;
-    
-    function Lottery() public {
+    address payable[]  public players;
+
+    constructor() {
         Admin = msg.sender;
     }
     
     function enter() public payable {
         require(msg.value > .01 ether);
-        players.push(msg.sender);
+        players.push(payable(msg.sender));
     }
     //Pick a winning address based on a sudo-random hash converted to initergers.
     function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+        return uint(keccak256(abi.encode(block.difficulty, block.timestamp, players)));
     }
     //pickWinner() picks the winning address, Only manager can call this function. After each round the player array is reset to 0.
     function pickWinner() public restricted {
         uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        players[index].transfer(address(this).balance);
+        delete players;
     }
     // Only manager block
     modifier restricted() {
@@ -28,7 +28,7 @@ contract Lottery {
         _;
     }
     //Return all the players who entered.
-    function getPlayers() public view returns (address[]) {
+    function getPlayers() public view returns (address payable[] memory) {
         return players;
     }
-}   
+}  
